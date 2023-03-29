3c4fbaf94ee28d65f93846584cb4408daa7fb8d8
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-03-29 09:49:13 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(thanks, event) {
+        const newMessage = [];
+        for (let t = 0; t < thanks.length; t++) {
+        if(t >= 0 ) {
+            newMessage.push(`Thank you, ${thanks[t]}, for the wonderful ${event} gift!`);
+    };
+    };
+
+    return newMessage;
+};
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--)
+    };
+};
+
+countDown(10)
